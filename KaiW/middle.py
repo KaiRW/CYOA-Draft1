bc4162722df8326fe550894bf40caa5ee674ec31
the Coder School <tcsbcoach@gmail.com>
2017-10-14 12:07:15 -0700

    Second Draft, combat works

--- KaiW/middle.py
+++ KaiW/middle.py
@@ -27,20 +27,45 @@
 
 # print(us(10983,3,53))
 
-class bobstats():
-	att = 7
-	defe= 9
-	hp=80
-	attpow= 2
+import random
 
-class blobstats():
-	att= 2
-	defe=4
-	hp=22
-	attpow=3
+class Custene:
+	def __init__(self,name,phrase,att,defe,hp,spd,attpow):
+		self.name= name
+		self.att = att
+		self.phrase= phrase
+		self.defe= defe
+		self.hp= hp
+		self.spd = spd
+		self.attpow =attpow
 
 def dam_value(attacker,defender):
 	return (defender.hp-((attacker.attpow*attacker.att)//defender.defe)),((attacker.attpow*attacker.att)//defender.defe)
 
-print("delt "+str(dam_value(bobstats,blobstats)[1])+" damage")
+def combat(X,Y):
+	if X.spd>Y.spd:
+		first = X
+		second = Y
+	if Y.spd>X.spd:
+		first = Y
+		second = X
+	print("I'm " + first.name + " " + enemy.phrase)
+	print("I'm " + second.name + " " + hero.phrase)
+	print(first.name+" delt " + str(dam_value(first, second)[1]) + " damage to " + second.name)
+	second.hp-=(dam_value(first, second)[1])
+	print(second.name+" has "+str(second.hp)+" left.")
+	print(second.name + " delt " + str(dam_value(second, first)[1]) + " damage to " + first.name)
+	first.hp -= (dam_value(second, first)[1])
+	print(first.name + " has " + str(first.hp) + " left.")
+
+
+
+enemy= Custene("Josh","and I didn't want the pizza",5,5,20,5,3)
+hero= Custene("Kai teh Bippo","and I would rather be playing video games",5,5,20,4,5)
+
+fight= combat(hero,enemy)
+
+# print("I'm "+enemy.name+" "+enemy.phrase)
+# print("I'm "+hero.name+" "+hero.phrase)
+# print("delt "+str(dam_value(enemy,hero)[1])+" damage to "+hero.name)
 
